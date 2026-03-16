d91cf9752cc19fbc55363b7ecbaf0f09e7bc08cb
GrinDi <Grin1990Di@gmail.com>
2026-03-16 22:49:30 +0300

    Добавил в файл __init__.py celery_app. Мы импортируем celery_app на уровне пакета, т.к.гарантируем, что Celery будет загружен и готов к использованию ещё до старта воркеров или задач.

--- python-final-diplom/reference/netology_pd_diplom/netology_pd_diplom/settings.py
+++ python-final-diplom/reference/netology_pd_diplom/netology_pd_diplom/settings.py
@@ -156,3 +156,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Celery settings
 CELERY_BROKER_URL = "redis://localhost:6379"
 CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+CELERY_ENABLE_UTC = True
