2802eca953aac0a5add7bd88063db07c9438706f
GrinDi <Grin1990Di@gmail.com>
2026-03-16 22:23:28 +0300

    Добавил в Settings.py: CELERY_BROKER_URL, CELERY_RESULT_BACKEND чтобы в Celery было достаточно информации, чтобы знать, куда отправлять сообщения и куда записывать результаты.

--- python-final-diplom/reference/netology_pd_diplom/netology_pd_diplom/settings.py
+++ python-final-diplom/reference/netology_pd_diplom/netology_pd_diplom/settings.py
@@ -152,3 +152,7 @@ REST_FRAMEWORK = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery settings
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
